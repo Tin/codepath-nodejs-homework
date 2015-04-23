f41fd9754ec72f0c46663add3b18d049290027a7
Tin (Yue Tian) <ytian0@walmartlabs.com>
2015-04-22 18:10:33 -0700

    add proxy server which request the echo server

--- 0-proxy-server/index.js
+++ 0-proxy-server/index.js
@@ -1,11 +1,25 @@
 let http = require('http')
+let request = require('request')
 
 http
-    .createServer((req, res) => {
-        console.log(`Request received at: ${req.url}`)
-        for (let header in req.headers) {
-            res.setHeader(header, req.headers[header])
-        }
-        req.pipe(res)
-    })
-    .listen(8000)
+.createServer((req, res) => {
+    console.log(`Request received at: ${req.url}`)
+    for (let header in req.headers) {
+        res.setHeader(header, req.headers[header])
+    }
+    req.pipe(res)
+})
+.listen(8000)
+
+http
+.createServer((req, res) => {
+    let destinationUrl = '127.0.0.1:8000'
+    console.log(`Proxying request to: ${destinationUrl + req.url}`)
+    let options = {
+        headers: req.headers,
+        url: `http://${destinationUrl}${req.url}`,
+        method: req.method
+    }
+    req.pipe(request(options)).pipe(res)
+})
+.listen(8001)
