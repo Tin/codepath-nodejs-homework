dcad521351450a4a942f0733cbdaf28e92621b48
Tin (Yue Tian) <ytian0@walmartlabs.com>
2015-04-22 18:41:58 -0700

    reorganize the code structure

--- 0-proxy-server/index.js
+++ 0-proxy-server/index.js
@@ -7,33 +7,46 @@ let scheme = 'http://'
 let port = argv.port || argv.host === '127.0.0.1' ? 8000 : 80
 let outputStream = argv.log ? fs.createWriteStream(argv.log) : process.stdout
 
-http
-.createServer((req, res) => {
-    console.log(`Request received at: ${req.url}`)
-    for (let header in req.headers) {
-        res.setHeader(header, req.headers[header])
-    }
-    req.pipe(res)
-})
-.listen(8000)
+let log = (message) => console.log(message)
 
-http
-.createServer((req, res) => {
-    let destinationUrl = req.headers['x-desination-url'] ||
-        argv.url ||
-        `${scheme}${argv.host}:${port}`
+let startEchoServer = (onPort) => {
+    http
+    .createServer((req, res) => {
+        log(`Request received at: ${req.url}`)
+        for (let header in req.headers) {
+            res.setHeader(header, req.headers[header])
+        }
+        req.pipe(res)
+    })
+    .listen(onPort)
 
-    console.log(`Proxying request to: ${destinationUrl + req.url}`)
+    log(`Echo server is listening on ${onPort}`)
+}
 
-    req.pipe(process.stdout)
-    let options = {
-        headers: req.headers,
-        url: `http://${destinationUrl}${req.url}`,
-        method: req.method
-    }
-    let downstreamResponse = req.pipe(request(options))
-    process.stdout.write('\n\n\n' + JSON.stringify(downstreamResponse.headers))
-    downstreamResponse.pipe(outputStream)
-    downstreamResponse.pipe(res)
-})
-.listen(8001)
+
+let startProxyServer = (onPort) => {
+    http
+    .createServer((req, res) => {
+        let destinationUrl = req.headers['x-desination-url'] ||
+            argv.url ||
+            `${scheme}${argv.host}:${port}`
+
+        log(`Proxying request to: ${destinationUrl + req.url}`)
+
+        req.pipe(process.stdout)
+        let options = {
+            headers: req.headers,
+            url: `http://${destinationUrl}${req.url}`,
+            method: req.method
+        }
+        let downstreamResponse = req.pipe(request(options))
+        log('\n\n\n' + JSON.stringify(downstreamResponse.headers))
+        downstreamResponse.pipe(outputStream)
+        downstreamResponse.pipe(res)
+    })
+    .listen(onPort)
+    log(`Proxy server is listening on ${onPort}`)
+}
+
+startEchoServer(8000)
+startEchoServer(8001)
