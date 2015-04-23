3aacec784d1061636833e2968d40124ce154b459
Tin (Yue Tian) <ytian0@walmartlabs.com>
2015-04-22 18:04:53 -0700

    upgrade it to echo server

--- 0-proxy-server/index.js
+++ 0-proxy-server/index.js
@@ -1,6 +1,11 @@
 let http = require('http')
 
-http.createServer((req, res) => {
-    console.log(`Request received at: ${req.url}`)
-    res.end('hello world\n')
-}).listen(8000)
+http
+    .createServer((req, res) => {
+        console.log(`Request received at: ${req.url}`)
+        for (let header in req.headers) {
+            res.setHeader(header, req.headers[header])
+        }
+        req.pipe(res)
+    })
+    .listen(8000)
